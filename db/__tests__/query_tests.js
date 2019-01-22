fab7d43550d6046e3661b481ca27f8b76289023c
mpmckenna8 <mpmckenna8@me.com>
2019-01-22 12:43:24 -0800

    Added a query to update the item quantity but as yet it is not used on the frontend

--- db/__tests__/query_tests.js
+++ db/__tests__/query_tests.js
@@ -2,7 +2,9 @@
 let get_all_items_collections = require('../config/queries/get_all_items_collections.js');
 let add_user_item = require('../config/queries/add_user_item.js')
 let get_user_items_and_collections = require('../config/queries/get_user_items_and_collections.js')
+let update_item_quantity = require('../config/queries/update_item_quantity.js')
 let delete_user_item = require('../config/queries/delete_user_item.js')
+
 
 let testItem =      { p_id: 151,
        name: 'testItemer',
@@ -10,7 +12,7 @@ let testItem =      { p_id: 151,
        weight: 2,
        value: null,
        category: 'other',
-       quantity: 2
+       quantity: 32
       };
 
 let testUser = {
@@ -22,11 +24,14 @@ let testUser = {
 get_all_items_collections( (items, collections) => { console.log('all items = ', items, 'all collections, ', collections) })
 
 
+
  //get_user_items_and_collections('test', (inventory, collections ) => {   console.log('test user inventory is: ', inventory, 'collections = ', collections) })
 
 
 //add_user_item( testItem, testUser, (res) => {  console.log('res from adding a user item', res.rows) })
 //console.log('need to delete user item');
 
+update_item_quantity( testUser.name, testItem, (msg) => { console.log('result of updateing item quanity,', msg)
+})
 
 //delete_user_item('test', 158, (res) => {   console.log('res from delete_user_item', res) })
---
+++ db/config/queries/update_item_quantity.js
@@ -0,0 +1,53 @@
+// a query to update the an itme quantity
+
+let pg = require('pg');
+
+var conString = "postgres://matthewmckenna@localhost/auth";
+
+let queryString = "select * from users where name=$1"
+
+
+
+let update_item_quantity = (userName, item, cb) => {
+
+  let client = new pg.Client(conString);
+
+  client.connect();
+  client.query(queryString, [userName], function(err, res) {
+    if(err) {
+      console.log('error updating qunat', err)
+      throw err
+    }
+
+    let userinfo = res.rows[0];
+
+    var userInventory = userinfo.inventory;
+    let itemIndex = userInventory.findIndex(function(d) {
+      //  console.log(d)
+      return (d === item.p_id);
+    })
+
+    let inventoryQuants = res.rows[0].inventoryquantity;
+
+    inventoryQuants[itemIndex] = item.quantity;
+
+    let updateString = 'UPDATE users SET inventoryquantity=$1 where name=$2 OR email=$2 returning inventoryquantity';
+
+    client.query(updateString, [inventoryQuants, userName ], function(err, res) {
+      if(err) {
+        console.log('error updating qunat', err)
+        throw err
+      }
+
+      console.log('should have updated item quantity its now', res.rows[0].inventoryquantity[itemIndex]);
+      cb('update should be successful')
+      client.end();
+
+    })
+
+
+  })
+
+}
+
+module.exports = update_item_quantity;
